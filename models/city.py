9e3b2467cf0b8e4c73c1346861ab8358f14835be
SafaFarah <kangjisook13@gmail.com>
2023-10-14 16:05:25 +0000

    adding city.py

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module contains City class."""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """a class City that inherits from BaseModel
+    Attributes:
+        state_id: (str) - State.id
+        name: (str) - city's mane
+    """
+    state_id = ""
+    name = ""
