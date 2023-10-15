b7a2f7d4c34b2a828a68e79e10b0a8e7c9946e2a
SafaFarah <kangjisook13@gmail.com>
2023-10-15 08:47:23 +0000

    adding test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -4,7 +4,6 @@ Test module for Amenity Class
 """
 from models.amenity import Amenity
 from datetime import datetime
-import json
 import models
 import unittest
 
@@ -13,7 +12,7 @@ class TestAmenity(unittest.TestCase):
     """Class for testing instantiation of the Amenity class."""
 
     def test_doc(self):
-        """... documentation for the class"""
+        """documentation for the class"""
         doc = Amenity.__doc__
         self.assertIsNotNone(doc)
 
@@ -25,53 +24,49 @@ class TestAmenity(unittest.TestCase):
     def test_type_attribute(self):
         """tests type of class  attributes"""
         self.assertEqual(str, type(Amenity.name))
-
+    
     def test_amenity_stored(self):
         """ tests amenity instance storged in storage"""
         self.assertIn(Amenity(), models.storage.all().values())
 
-    def test_unique_amenitys(self):
-        """ tests Amenity instances have unique id"""
-        amenity1 = Amenity()
-        amenity2 = Amenity()
-        self.assertNotEqual(amenity1.id, amenity2.id)
-
     def test_None_arg(self):
         """ pass None as arguments"""
-        amenity3 = Amenity(None)
-        self.assertNotIn(None, amenity3.__dict__.values())
+        amenity = Amenity(None)
+        self.assertNotIn(None, amenity.__dict__.values())
 
     def test_attr(self):
         """Test Amenity instances have attributes"""
         amenity = Amenity()
-        self.assertTrue(hasattr(amenity, "name"))
-        self.assertTrue(hasattr(amenity, "id"))
-        self.assertTrue(hasattr(amenity, "created_at"))
-        self.assertTrue(hasattr(amenity, "updated_at"))
+        self.assertTrue(hasattr(Amenity, "name"))
 
     def test_Amenity_to_dict(self):
         """Test the dictionery of a Amenity instance"""
         time = datetime.today()
         amenity = Amenity()
-        amenity.id = "1234-753223-18932"
-        amenity.name = "jisook"
+        amenity.name = "Piscina"
+        amenity.id = "122234"
         amenity.created_at = amenity.updated_at = time
         amenity_dict = {
-            'id': '1234-753223-18932',
+            'name': 'Piscina',
+            'id': '122234',
             '__class__': 'Amenity',
             'created_at': time.isoformat(),
-            'updated_at': time.isoformat(),
-            'name': "jisook",
+            'updated_at': time.isoformat()
         }
-
         self.assertDictEqual(amenity.to_dict(), amenity_dict)
+
+    def test_unique_amenitys(self):
+        """ tests User instances have unique id"""
+        amenity1 = Amenity()
+        amenity2 = Amenity()
+        self.assertNotEqual(amenity1.id, amenity2.id)
 
     def test_file_update(self):
         """ tests update of file with instance"""
         amenity = Amenity()
         amenity.save()
         amenity_id = "Amenity." + amenity.id
-        with open(r"models/engine/file.json", "r") as file:
+        with open("file.json", "r") as file:
             self.assertIn(amenity_id, file.read())
 
     def test_dict(self):
@@ -79,6 +74,10 @@ class TestAmenity(unittest.TestCase):
         amenity = Amenity()
         self.assertNotEqual(amenity.to_dict(), amenity.__dict__)
 
+    def test_None_kwargs(self):
+        """ Test passes None as kwargs"""
+        with self.assertRaises(TypeError):
+            Amenity(id=None, created_at=None, updated_at=None)
 
 if __name__ == "__main__":
     unittest.main()
