13cd730f24325fe35f9ca4159a4ce7065ac58b14
mojtabababiker <mojmohammad98@gmail.com>
2023-10-15 18:14:20 +0300

    Edite BnB console, fixed bugs on the non-interactive session, version 0.3.2

--- console.py
+++ console.py
@@ -21,7 +21,13 @@ import sys
 import re
 import json
 from models import storage
-from models.models import *
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
